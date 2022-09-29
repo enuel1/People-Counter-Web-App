24632eee966290c3f56f1a2a3ecfcad5a0895c19
enuel1 <58036500+enuel1@users.noreply.github.com>
2022-09-29 16:48:22 +0100

    Add all the files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+
+
+let countEl = document.getElementById ("count-el")
+let entriesEl = document.getElementById ("entries-el")
+let count = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    entriesEl.textContent += count + " - "
+    countEl.textContent = 0
+    count = 0
+
+}
+
